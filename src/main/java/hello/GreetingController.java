37ff68a2030ae4e5f2b9c00f301c4067e0482b3e
mlyadav <malti.yadav@adtran.com>
2018-07-24 13:15:25 +0530

    Initial commit

---
+++ src/main/java/hello/GreetingController.java
@@ -0,0 +1,23 @@
+package hello;
+
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController{
+    private static final String template = "Hello, %s!";
+    public final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name){
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+
+    @RequestMapping("/")
+    public Index greeting1(@RequestParam(value = "name", defaultValue = "World") String name){
+        return new Index(String.format(template, name));
+    }
+
+}
---
+++ src/main/java/hello/Index.java
@@ -0,0 +1,10 @@
+package hello;
+
+public class Index{
+    public final String content;
+
+    public Index(String content){
+        this.content = content;
+    }
+
+}
